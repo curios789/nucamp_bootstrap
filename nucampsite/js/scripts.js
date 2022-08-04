f62351e72b3084bebc55a9308d749cd900387ccb
David Umstot <umstot.david@gmail.com>
2022-08-04 13:59:48 -0500

    Final commit

---
+++ nucampsite/js/scripts.js
@@ -0,0 +1,20 @@
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").on("click", function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+    $('#loginButton').on("click", function () {
+        $('#loginModal').modal();
+    });
+    $('#reserveButton').on("click", function () {
+        $('#reserveModal').modal();
+    });
+});
